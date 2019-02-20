4c757ab6df2d348d5301d929860230879f4477a6
Rick Selby <rick@selby-family.co.uk>
2019-02-20 17:47:43 +0000

    Remove php files

--- src/rtw/archive.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (isset($_GET['m'])) {
-    header('Location: archive.php%3Fm=' . $_GET['m'] . '.html');
-} else {
-    header('Location: archive.php.html');
-}
-
-
--- src/rtw/worldmap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (isset($_GET['m'])) {
-    header('Location: worldmap.php%3Fm=' . $_GET['m'] . '.html');
-} else {
-    header('Location: worldmap.php.html');
-}
-
-
